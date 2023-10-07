e85fa3e5ba104897034988a68baeae24f656fbdf
Aswin Logan <a24logan@edu.uwaterloo.ca>
2023-10-07 17:17:57 -0400

    directory done

---
+++ src/components/directory/index.js
@@ -0,0 +1,13 @@
+import "./directory.styles.scss";
+import CategoryItem from "../category-item";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category}></CategoryItem>
+      ))}
+    </div>
+  );
+};
+export default Directory;
