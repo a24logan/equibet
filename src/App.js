e8af505a7126ad3e6693a4bef8950bdcb9de50c7
Aswin Logan <a24logan@edu.uwaterloo.ca>
2023-10-12 00:11:06 -0400

    finihed nav

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
-import Directory from "./components/directory";
-
+import Home from "./routes/home/home.component";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
   return (
-    <div>
-      <Directory categories={categories}></Directory>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigation></Navigation>}>
+        <Route path="" index element={<Home></Home>}></Route>
+        <Route path="shop" index element={<div>Shop Page</div>}></Route>
+        <Route path="sign-in" index element={<SignIn></SignIn>}></Route>
+      </Route>
+    </Routes>
   );
 };
 
