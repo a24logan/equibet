52d98f7177effcf4d1d4c20ee4dcc7fdbe5c4e49
Aswin Logan <a24logan@edu.uwaterloo.ca>
2023-10-24 00:22:43 -0400

    adding remaining route stuff

--- src/components/directory/index.js
+++ src/components/directory/index.js
@@ -1,11 +1,11 @@
 import "./directory.styles.scss";
-import CategoryItem from "../category-item";
+import DirectoryItem from "../directory-item";
 
 const Directory = ({ categories }) => {
   return (
     <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem key={category.id} category={category}></CategoryItem>
+        <DirectoryItem key={category.id} category={category}></DirectoryItem>
       ))}
     </div>
   );
